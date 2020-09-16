1bdd73e07ed748fd39566ef24ecd92b47c34dee6
youchao liu <youchaoliu@xiaohongshu.com>
2020-09-16 17:33:20 +0800

    close #14: load strategy on halationState level

--- stories/Basic/plugin-b/register.ts
+++ stories/Basic/plugin-b/register.ts
@@ -3,20 +3,5 @@ export default function PluginComponent() {
     name: 'plugin-b',
     getModel: () => require('./model'),
     getComponent: () => require('./index'),
-
-    strategies: [{
-      type: 'runtime',
-      resolver: (props) => {
-        const { count } = props
-        return count === 2
-      }
-    }, {
-      type: 'event',
-      resolver: ({ event }) => {
-        const { imageLoaded } = event
-        if (imageLoaded) return true
-        return false
-      }
-    }]
   };
 }
