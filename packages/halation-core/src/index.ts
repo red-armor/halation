3be64a631ab48178d120a516c674d75ad6249f0c
youchao liu <youchaoliu@xiaohongshu.com>
2021-02-19 16:14:04 +0800

    feat: update package.json

---
+++ packages/halation-core/src/index.ts
@@ -0,0 +1,13 @@
+import * as utils from './commons/utils'
+
+export { default as HalationBase } from './HalationBase'
+export { default as LoadManagerBase } from './LoadManagerBase'
+export { default as BlockWrapper } from './BlockWrapper'
+export { default as RecordBase } from './data/RecordBase'
+export { default as ModuleBase } from './ModuleBase'
+export { default as Loader } from './Loader'
+
+export * from './types';
+export * from './commons/logger';
+
+export { utils }
